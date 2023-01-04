04178d778051c8073acc34cc7d9469d05cfbfee8
Kwon-sang <kwonsg514@gmail.com>
2023-01-04 23:20:27 +0900

    낮은 결합도(인터페이스 활용)

--- src/chap02_입출금내역분석기/BankStatementAnalyzer.java
+++ src/chap02_입출금내역분석기/BankStatementAnalyzer.java
@@ -4,24 +4,18 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.time.LocalDate;
 import java.time.Month;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
 
-public class BankTransactionAnalyzerSimple {
+public class BankStatementAnalyzer {
 
     private static final String RESOURCES = "src/main/resources/";
-    private static final BankStatementCSVParser bankStatementParser = new BankStatementCSVParser();
 
-    public static void main(String[] args) throws IOException {
-
-        final String fileName = args[0];
+    public void analyze(final String fileName, final BankStatementParser bankStatementParser) throws IOException {
         final Path path = Paths.get(RESOURCES + fileName);
         final List<String> lines = Files.readAllLines(path);
 
-        final List<BankTransaction> bankTransactions = bankStatementParser.parseLinesFromCSV(lines);
+        final List<BankTransaction> bankTransactions = bankStatementParser.parseLinesFrom(lines);
         final BankStatementProcessor bankStatementProcessor = new BankStatementProcessor(bankTransactions);
 
         collectSummary(bankStatementProcessor);
---
+++ src/chap02_입출금내역분석기/BankStatementParser.java
@@ -0,0 +1,9 @@
+package chap02_입출금내역분석기;
+
+import java.util.List;
+
+public interface BankStatementParser {
+
+    BankTransaction parseFrom(String line);
+    List<BankTransaction> parseLinesFrom(List<String> lines);
+}
